8cbcfccb74332813d27aaec3616e7ce22ea390d5
Arya Chowdhury <acschool.445@gmail.com>
2023-08-15 13:05:22 -0400

    Initial Commit

---
+++ GitHubPractice/GitHubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPracticeApp.swift
+//  GitHubPractice
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
